6659fbdda1f53802ea46f370b7d381c9fe8dad02
Александр Мосиевских <rvzdvv@yandex.ru>
2022-12-27 09:38:22 +0000

    Update store.go

--- backend/internal/store/dumplings/fake/store.go
+++ backend/internal/store/dumplings/fake/store.go
@@ -6,7 +6,7 @@ import (
 	"sync/atomic"
 	"time"
 
-	"gitlab.praktikum-services.ru/Stasyan/momo-store/internal/store/dumplings"
+	"gitlab.praktikum-services.ru/a.mosievskih/momo-store/internal/store/dumplings"
 )
 
 // Store is a fake in-memory implementation of dumplings.Store
