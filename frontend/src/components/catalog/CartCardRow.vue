6329933658a3d263099cb49fdd36b6f14daec9a8
Александр Мосиевских <rvzdvv@yandex.ru>
2023-01-04 05:58:53 +0000

    Update CartCardRow.vue

--- frontend/src/components/catalog/CartCardRow.vue
+++ frontend/src/components/catalog/CartCardRow.vue
@@ -1,16 +1,19 @@
 <template>
-  <li 
-    class="list-group-item d-flex justify-content-between align-items-center"
-    @mouseenter="hovering = true"
-    @mouseleave="hovering = false"
-  >
-    <span>{{ cartItem.product.name }}</span>
-    <div class="right-section">
-      <button v-if="hovering" @click="removeFromCart" class="btn btn-close"></button>
-      <span v-if="!hovering" class="bg-primary quantity">{{ cartItem.quantity }}</span>
-    </div>
-  </li>
+  <ul>
+    <li 
+      class="list-group-item d-flex justify-content-between align-items-center"
+      @mouseenter="hovering = true"
+      @mouseleave="hovering = false"
+    >
+      <span>{{ cartItem.product.name }}</span>
+      <div class="right-section">
+        <button v-if="hovering" @click="removeFromCart" class="btn btn-close"></button>
+        <span v-if="!hovering" class="bg-primary quantity">{{ cartItem.quantity }}</span>
+      </div>
+    </li>
+  </ul>
 </template>
+
 
 <script lang="ts">
 import { defineComponent } from 'vue'
